8cbd5b90171dffad60a98a863e498317d26362ca
Pickachu Chen <pickachu1006@gmail.com>
2020-04-24 16:56:08 +0800

    20200424_mother's day ig_wall RWD_2

--- js/all.js
+++ js/all.js
@@ -18,41 +18,4 @@ $(document).ready(function () {
             scrollTop: $('#size').position().top,
         }, 200);
     });
-
-    // $(window).scroll(function () {
-    //     var scrollPos = $(window).scrollTop();
-    //     var windowHeight = $(window).height();
-    //     var sizeTop = $('#size').position().top;
-    //     var width = $(window).width();
-
-    //     console.log("scrollPos:"+scrollPos,"windowHeight:"+windowHeight,"sizeTop:"+sizeTop,"width:"+width);
-    //     // mac scrollPos:25003 windowHeight:363 sizeTop:25029.3125 width:1280
-    //     // iphone scrollPos:18815 windowHeight:667 sizeTop:18817.21875 width:375
-    //     // ipad scrollPos:33844 windowHeight:1024 sizeTop:33832.578125 width:768
-
-    // });
-    // var windowHeight = $(window).height();
-    // var posTop = $('#size').position().top;
-    // // var offsetTop = $('#size').offset().top;
-    // var width = $(window).width();
-
-    // if (width <= 768) {
-    //     // 立即訂購btn動態
-    //     $('.buybtn').click(function (e) {
-    //         e.preventDefault();
-    //         $('html , body').animate({
-    //             // scrollTop: 25372,
-    //             scrollTop: $('#size').position().top,
-    //         }, 900);
-    //     });
-    // } else if (width > 768) {
-    //     // 立即訂購btn動態
-    //     $('.buybtn').click(function (e) {
-    //         e.preventDefault();
-    //         $('html , body').animate({
-    //             // scrollTop: posTop,
-    //             scrollTop: $('#size').position().top,
-    //         }, 900);
-    //     });
-    // }
 });
